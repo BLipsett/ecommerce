7e0f5294490ef1e1a0e32691e941531a9053ebf7
Brian Lipsett <lipsettbrian3@gmail.com>
2022-03-28 12:13:37 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
-import HomepageComponent from './pages/homepage/homepage.component'
+import { Switch } from 'react-router-dom';
+//import { Router, Route, Switch } from "react-router";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import HomepageComponent from './pages/homepage/homepage.component';
+import ShopView from './pages/shop/shop.component';
+
+
 
 function App() {
   return (
     <div>
-      <HomepageComponent />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={HomepageComponent} />
+          <Route path='/shop' component={ShopView} />
+        </Switch>
+      </Router>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/preview-collection/preview.collection.component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './preview.style.scss';
+
+
+const CollectionPreview = () => (
+    
+)
